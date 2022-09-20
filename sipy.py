35af1595f8f1d88cea86184c532d5a65df794ece
NicholasTTF <112996489+NicholasTTF@users.noreply.github.com>
2022-09-20 11:51:31 +0800

    + added 2-sample t-test (independent samples) assuming equal variance, 2-sample t-test (independent samples) assuming unequal variance, t-test - paired (dependent samples)

--- sipy.py
+++ sipy.py
@@ -345,12 +345,34 @@ class SiPy_Shell(object):
         Performs Student's t-test(s) on values.
 
         Commands:
-            ttest 1s <variable name> <population mean>
+            ttest 1s {list|series|tuple|vector} <variable name> <population mean>
+            ttest 2se {list|series|tuple|vector} <variable name A> <variable name B>
+            ttest 2su {list|series|tuple|vector} <variable name A> <variable name B>
+            ttest paired {list|series|tuple|vector} <variable name A> <variable name B>
         """
+        data_type = operand[1].lower()
         if operand[0].lower() in ["1s", "1sample"]:
-            data_values = self.data[operand[1]]
-            mu = float(operand[2])
-            retR = libsipy.base.tTest1Sample(data_values, mu)
+            data_values = self.data[operand[2]]
+            mu = float(operand[3])
+            if data_type in ["list", "series", "tuple", "vector"]:
+                retR = libsipy.base.tTest1Sample(data_values, mu)
+            elif data_type in ["dataframe", "df", "frame", "table"]:
+                pass
+        elif operand[0].lower() in ["2se", "2sample_equal"]:
+            data_valuesA = self.data[operand[2]]
+            data_valuesB = self.data[operand[3]]
+            if data_type in ["list", "series", "tuple", "vector"]:
+                retR = libsipy.base.tTest2SampleEqual(data_valuesA, data_valuesB)
+        elif operand[0].lower() in ["2su", "2sample_unequal"]:
+            data_valuesA = self.data[operand[2]]
+            data_valuesB = self.data[operand[3]]
+            if data_type in ["list", "series", "tuple", "vector"]:
+                retR = libsipy.base.tTest2SampleUnequal(data_valuesA, data_valuesB)
+        elif operand[0].lower() in ["paired", "dependent"]:
+            data_valuesA = self.data[operand[2]]
+            data_valuesB = self.data[operand[3]]
+            if data_type in ["list", "series", "tuple", "vector"]:
+                retR = libsipy.base.tTest2SamplePaired(data_valuesA, data_valuesB )
         else: 
             retR = "Unknown sub-operation: %s" % operand[0].lower()
         print(retR)
