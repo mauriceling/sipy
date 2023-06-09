82bf071c103dde1298e1b55fb30f2bf58310c583
Maurice HT Ling <mauriceling@acm.org>
2023-06-09 14:26:31 +0800

    + added keyword method for variance levene

--- sipy.py
+++ sipy.py
@@ -956,7 +956,9 @@ class SiPy_Shell(object):
             variance fligner {dataframe|df|frame|table} wide data=<variable name>
 
             variance levene {list|series|tuple|vector} <variable name 1> <variable name 2> ... <variable name N>
+            variance levene {list|series|tuple|vector} data=<variable name 1>;<variable name 2>; ... ;<variable name N>
             variance levene {dataframe|df|frame|table} wide <variable name>
+            variance levene {dataframe|df|frame|table} wide data=<variable name>
 
         @return: String containing results of command execution
         """
@@ -1003,13 +1005,22 @@ class SiPy_Shell(object):
                 retR = "Statistic = %.3f; p-value = %s" % (result.statistic, result.pvalue)
         elif operand[0].lower() in ["levene"]:
             if data_type in ["list", "series", "tuple", "vector"]:
-                # variance levene {list|series|tuple|vector} <variable name 1> <variable name 2> ... <variable name N>
-                data_values = [self.data[operand[i]] for i in range(2, len(operand))]
+                if len(kwargs) == 0:
+                    # variance levene {list|series|tuple|vector} <variable name 1> <variable name 2> ... <variable name N>
+                    data_values = [self.data[operand[i]] for i in range(2, len(operand))]
+                else:
+                    # variance levene {list|series|tuple|vector} data=<variable name 1>;<variable name 2>; ... ;<variable name N>
+                    varNs = [x.strip() for x in kwargs["data"].split(";")]
+                    data_values = [self.data[var] for var in varNs]
                 result = libsipy.base.LeveneTest(data_values)
                 retR = "Statistic = %.3f; p-value = %s" % (result.statistic, result.pvalue)
             elif data_type in ["dataframe", "df", "frame", "table"] and operand[2].lower() == "wide":
-                # variance levene {dataframe|df|frame|table} wide <variable name>
-                data_values = libsipy.data_wrangler.df_extract(self.data[operand[3]], columns="all", rtype="list")
+                if len(kwargs) == 0:
+                    # variance levene {dataframe|df|frame|table} wide <variable name>
+                    data_values = libsipy.data_wrangler.df_extract(self.data[operand[3]], columns="all", rtype="list")
+                else:
+                    # variance levene {dataframe|df|frame|table} wide data=<variable name>
+                    data_values = libsipy.data_wrangler.df_extract(self.data[kwargs["data"]], columns="all", rtype="list")
                 result = libsipy.base.LeveneTest(data_values)
                 retR = "Statistic = %.3f; p-value = %s" % (result.statistic, result.pvalue)
         else: 
