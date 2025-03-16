b2406a2c016554f58e1038e2c9a0ada6b1b126f8
Maurice Ling <mauriceling@acm.org>
2025-03-16 13:51:23 +0800

    + added R 4.4.1 as portable R

---
+++ portable_R/include/Rversion.h
@@ -0,0 +1,29 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 263169
+#define R_NICK "Race for Your Life"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "4"
+#define R_MINOR  "4.1"
+#define R_STATUS ""
+#define R_YEAR   "2024"
+#define R_MONTH  "06"
+#define R_DAY    "14"
+#define R_SVN_REVISION 86737
+#ifdef __llvm__
+# define R_FILEVERSION    4,41,21201,0
+#else
+# define R_FILEVERSION    4,41,86737,0
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
