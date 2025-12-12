80215e958dbefe8432128735e467309415e19ecb
Maurice Ling <mauriceling@acm.org>
2025-12-12 14:23:20 +0800

    # updated status to draft - in preparation for v0.8.0

--- sipy_info.py
+++ sipy_info.py
@@ -19,7 +19,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-draft = False
+draft = True
 
 release_number = "0.7.0"
 release_code_name = "Keropok"
